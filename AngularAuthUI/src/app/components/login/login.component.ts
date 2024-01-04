beb19924ba8fa28cc1a6136b46ba04bc45a247e2
AndriySham <avshamatrin@gmail.com>
2024-01-04 18:37:10 +0200

    Implemented validation in Login and SignUp components. Remove the some code in validateform.ts file

--- AngularAuthUI/src/app/components/login/login.component.ts
+++ AngularAuthUI/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
+import { ValidateForm } from 'src/app/helpers/validateform';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,16 +11,29 @@ export class LoginComponent implements OnInit{
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash";
+  loginForm!: FormGroup;
 
-  constructor(){}
+  constructor(private fb: FormBuilder){}
 
   ngOnInit(): void {
-
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
   hideShowPass(){
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      // Send form to database
+    } else {
+      ValidateForm.validateAllFormFields(this.loginForm);
+      alert("Your form is invalid");
+    }
   }
 }
--- AngularAuthUI/src/app/components/signup/signup.component.ts
+++ AngularAuthUI/src/app/components/signup/signup.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateform';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash";
+  signUpForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){ }
+
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      userName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
 
   hideShowPass(){
     this.isText = !this.isText;
@@ -16,4 +31,12 @@ export class SignupComponent {
     this.isText ? this.type = "text" : this.type = "password";
   }
 
+  onSignup(){
+    if(this.signUpForm.valid){
+      // Send form to database
+    } else {
+      ValidateForm.validateAllFormFields(this.signUpForm);
+      alert("Your form is invalid");
+    }
+  }
 }
